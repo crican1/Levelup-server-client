5a7c8b3822c8457fc82ed66e98f66372b91d11f0
Claudio Villalobos <claudiodvillalobos@gmail.com>
2023-06-03 11:09:55 -0500

    Adding some functions into gameData

--- utils/data/gameData.js
+++ utils/data/gameData.js
@@ -8,15 +8,23 @@ const getGames = () => new Promise((resolve, reject) => {
 });
 
 const createGame = (game) => new Promise((resolve, reject) => {
-  fetch("", {})
-    .then()
-    .catch();
+  fetch(`${clientCredentials.databaseURL}/games`, {
+    method: 'Post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(game),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
 });
 
 const getGameTypes = () => new Promise((resolve, reject) => {
-  fetch("", {})
-    .then()
-    .catch();
+  fetch(`${clientCredentials.databaseURL}/gametypes`, {})
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
 });
 
 // eslint-disable-next-line import/prefer-default-export
